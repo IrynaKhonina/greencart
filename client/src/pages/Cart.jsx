55447bf2109612a976a72031d1e86b04bc2914c2
IrynaKhonina <irinahonina@yahoo.com>
2025-05-14 22:48:30 +0300

    Cart/-- start

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -110,7 +110,7 @@ export const Cart = () => {
                             `${selectedAddress.street}, 
                             ${selectedAddress.city}, 
                             ${selectedAddress.state}, 
-                            ${selectedAddress.country}`:"No address found"}</p>
+                            ${selectedAddress.country}` : "No address found"}</p>
 
                         <button onClick={() => setShowAddress(!showAddress)}
                                 className="text-indigo-500 hover:underline cursor-pointer">
@@ -118,48 +118,57 @@ export const Cart = () => {
                         </button>
                         {showAddress && (
                             <div className="absolute top-12 py-1 bg-white border border-gray-300 text-sm w-full">
-                                <p onClick={() => setShowAddress(false)}
-                                   className="text-gray-500 p-2 hover:bg-gray-100">
-                                    New York, USA
-                                </p>
-                                <p onClick={() => setShowAddress(false)}
-                                   className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-indigo-500/10">
-                                    Add address
-                                </p>
+
+                                {addresses.map((address, index) => (
+                                    <p onClick={() => { setSelectedAddress(address);
+                                        setShowAddress(false)}}
+                                       className="text-gray-500 p-2 hover:bg-gray-100">
+                                        {address.street},
+                                        {address.city},
+                                        {address.state},
+                                        {address.country}
+                                    </p>
+                                    ))}
+
+                                        <p onClick={() => navigate("/add-address")}
+                                           className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-indigo-500/10">
+                                            Add address
+                                        </p>
+                                    </div>
+                            )}
                             </div>
-                        )}
-                    </div>
 
-                    <p className="text-sm font-medium uppercase mt-6">Payment Method</p>
+                            <p className="text-sm font-medium uppercase mt-6">Payment Method</p>
 
-                    <select className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none">
-                        <option value="COD">Cash On Delivery</option>
-                        <option value="Online">Online Payment</option>
-                    </select>
-                </div>
+                            <select onChange={e=>setPaymentOptions(e.target.value)}
+                                    className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none">
+                            <option value="COD">Cash On Delivery</option>
+                            <option value="Online">Online Payment</option>
+                            </select>
+                            </div>
 
-                <hr className="border-gray-300"/>
+                            <hr className="border-gray-300"/>
 
-                <div className="text-gray-500 mt-4 space-y-2">
-                    <p className="flex justify-between">
-                        <span>Price</span><span>$20</span>
-                    </p>
-                    <p className="flex justify-between">
-                        <span>Shipping Fee</span><span className="text-green-600">Free</span>
-                    </p>
-                    <p className="flex justify-between">
-                        <span>Tax (2%)</span><span>$20</span>
-                    </p>
-                    <p className="flex justify-between text-lg font-medium mt-3">
-                        <span>Total Amount:</span><span>$20</span>
-                    </p>
-                </div>
+                            <div className="text-gray-500 mt-4 space-y-2">
+                            <p className="flex justify-between">
+                            <span>Price</span><span>{currency}{getCartAmount()}</span>
+                            </p>
+                            <p className="flex justify-between">
+                            <span>Shipping Fee</span><span className="text-green-600">Free</span>
+                            </p>
+                            <p className="flex justify-between">
+                            <span>Tax (2%)</span><span>{currency}{getCartAmount()*2/100}</span>
+                            </p>
+                            <p className="flex justify-between text-lg font-medium mt-3">
+                            <span>Total Amount:</span><span>{currency}{getCartAmount() + getCartAmount()*2/100}</span>
+                            </p>
+                            </div>
 
-                <button
-                    className="w-full py-3 mt-6 cursor-pointer bg-indigo-500 text-white font-medium hover:bg-indigo-600 transition">
-                    Place Order
-                </button>
-            </div>
-        </div>
-    ) : null
-}
+                            <button
+                            className="w-full py-3 mt-6 cursor-pointer bg-indigo-500 text-white font-medium hover:bg-indigo-600 transition">
+                                {paymentOptions === "COD" ? "Place Order" : "Proceed to Checkout"}
+                            </button>
+                            </div>
+                            </div>
+                            ) : null
+                        }
